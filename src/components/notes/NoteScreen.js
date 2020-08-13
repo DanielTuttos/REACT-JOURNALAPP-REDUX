e38f60005bb60de2795c94f611ab87fe9bba2621
DanielTuttos <tutorialesdanielromero@gmail.com>
2020-08-13 18:53:28 -0500

    Firts Commit: section 17 finished - SASS - Structure and design

---
+++ src/components/notes/NoteScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NotesAppBar } from './NotesAppBar';
+
+export const NoteScreen = () => {
+    return (
+        <div className="notes__main-content">
+            <NotesAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happened today?"
+                    className="notes__textarea"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img
+                        src="https://ep01.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572453030_noticia_normal.jpg"
+                        alt="imagen"
+                    />
+                </div>
+
+            </div>
+        </div>
+    )
+}
